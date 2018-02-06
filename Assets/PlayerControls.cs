b3d70d07959fd9f5ec3cd4dc54ab47bc78c96c68
cevau <cevau@posteo.de>
2018-02-06 19:40:41 +0100

    better movement on moving platforms
    - smoothed movement on elevators (up/down)
    - player movement fits with moving platforms (platforms need to be tagged as "platforms" to lock the player to them)
    - to use moving platforms, create empty object with movingObject.cs, put platforms as child. the player must not be child of the platform itself
    - changed gravity to -50

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour {
 
 	public int speed = 450;
-	public int resetFallingHeight = 50;
+	public int maxFallingHeight = 50;
 
-	private GameObject movementPivot;
+	private GameObject pivot;
 
 	private Vector3 spawnPoint;
 	private Vector3 rotAxis;
@@ -21,41 +21,12 @@ public class PlayerControls : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		spawnPoint = transform.position;
-		movementPivot = transform.Find ("MovementPivot").gameObject;
+		pivot = new GameObject("Pivot");
+		pivot.transform.SetParent (transform);
 		AlignPosition ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//ground detection
-		RaycastHit[] hit;
-		hit = Physics.RaycastAll (transform.position, new Vector3 (0, -1, 0), 1f);
-		if (hit.Length > 0) {
-			falling = false;
-		} else {
-			falling = true;
-		}
-		//player is within a movement
-		if (moving) {
-			//continue 90-degree-rotation for this frame
-			transform.RotateAround (movementPivot.transform.position, rotAxis, speed * Time.deltaTime);
-			if (transform.position.y <= Mathf.Round (transform.position.y)) {
-				moving = false;
-				//position correction
-				//transform.eulerAngles = Vector3.zero;
-				//transform.position = new Vector3 (Mathf.Round (transform.position.x), height, Mathf.Round (transform.position.z));
-				AlignPosition();
-			}
-		// player is free falling
-		} else if (falling) {
-			if (transform.position.y < lowerBound) {
-				//reset to active checkpoint
-				transform.position = spawnPoint;
-				AlignPosition();
-				falling = false;
-			}
-		//player interaction is not locked by a current falling or moving status
-		} else {
+	void Update (){
+		if (!moving && !falling) {
 			AlignPosition ();
 			if (Input.GetKeyDown (KeyCode.Keypad1)) {
 				//x-0.5, y-0.5 BottomLeft
@@ -78,15 +49,50 @@ public class PlayerControls : MonoBehaviour {
 				rotAxis = Vector3.back;
 				moving = true;
 			}
-			movementPivot.transform.localPosition = rotPointOffset;
+			pivot.transform.localPosition = rotPointOffset;
 		}
+	}
+
+	void FixedUpdate () {
+		//ground detection
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, Vector3.down, out hit, 0.51f)) {
+			falling = false;
+			if (hit.transform.tag == "Platform") {
+				transform.SetParent (hit.transform.parent);
+			}
+		} else {
+			falling = true;
+		}
+		
+		//player is within a movement
+		if (moving) {
+			//continue 90-degree-rotation for this frame
+			transform.RotateAround (pivot.transform.position, rotAxis, speed * Time.deltaTime);
+			if (transform.localPosition.y <= Mathf.Round (transform.localPosition.y)) {
+				moving = false;
+				AlignPosition ();
+			}
+			// player is free falling
+		} else {
+			
+		}
+		if (falling) {
+			if (transform.position.y < lowerBound) {
+				//reset to active checkpoint
+				transform.position = spawnPoint;
+				AlignPosition();
+				falling = false;
+			}
+		//player interaction is not locked by a current falling or moving status
+		} 
 	}
 	//corrects the position of the game object to integers, sets all angles and velocity to zero 
 	public void AlignPosition(){
-		transform.rotation = Quaternion.identity;
-		transform.localPosition = new Vector3 (Mathf.Round (transform.localPosition.x), Mathf.Round (transform.localPosition.y), Mathf.Round (transform.localPosition.z));
+		transform.localRotation = Quaternion.identity;
+		transform.localPosition = new Vector3 (Mathf.Round (transform.localPosition.x), transform.localPosition.y, Mathf.Round (transform.localPosition.z));
 		gameObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
-		lowerBound = (int)transform.position.y - resetFallingHeight;
+		lowerBound = (int)transform.position.y - maxFallingHeight;
 	}
 	public void SetSpawnPoint(Vector3 spawnPoint){
 		this.spawnPoint = spawnPoint;

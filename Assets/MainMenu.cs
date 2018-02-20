ecb824678b499c825d847a57b214ee4b1ee59819
taravancalster <123elefante>
2018-02-20 15:25:01 +0100

    added play function
    if the play button from the main menu is clicked it will open the next scene (in this case TS Tower & Moving Plattform)

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame (){
+	//	SceneManager.LoadScene(1); //Lädt eine bestimmte Szene
+	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Lädt die nächste Szene in der Warteschlange, dafür müssen wir sicher gehen das diese sich dort befindet (File -> Build Settings.. -> Scenes in Build) 
+	}
+}

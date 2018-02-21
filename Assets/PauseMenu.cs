4e84a940ff352f7e0b4fb419d1363905f7a2e9d6
taravancalster <123elefante>
2018-02-21 19:34:09 +0100

    added pause menu
    added a pause menu in the TS Tower & Moving Plattform scene which allows to resume the game, quit the sound and go back to the main menu
    there are still bugs when going from the main menu to the scene

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	
+	public GameObject pauseMenuUI; 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Resume (){
+		pauseMenuUI.SetActive(false); //Das Pausenmenü ist nicht sichtbar
+		Time.timeScale = 1f; //Somit geht das Spiel wieder weiter
+		GameIsPaused = false;
+	}
+	
+	public void Pause (){
+		pauseMenuUI.SetActive(true); //Macht das Pausenmenü sichtbar
+		Time.timeScale = 0f; //Dient dazu die Zeit in dem Spiel anzuhalten
+		GameIsPaused = true; 
+	}
+	
+	public void LoadMenu(){
+		SceneManager.LoadScene(0);	//Ruft Szene 0 auf, das ist in unserem Fall die Menu Szene
+	}
+	
+}

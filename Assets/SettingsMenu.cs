8cf0637a20b4b0f09d1d7c2ee3532ca3479646ba
Nana <tietje.hh@gmail.com>
2018-02-21 21:46:28 +0100

    Colouritem changes

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+	public AudioMixer audioMixer;
+
+	public void SetMusic (float music){
+		audioMixer.SetFloat("music", music);		
+	}
+	
+	public void SetSound (float sound){
+		audioMixer.SetFloat("sound", sound);		
+	}
+
+}

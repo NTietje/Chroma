d71457b723bf0efa34c462bac1f095b74460f803
taravancalster <123elefante>
2018-02-22 10:31:55 +0100

    fixed play bug
    whenever the player goes back to the main menu from the game pause menu and then back again into the game it works normally

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+
 	public void PlayGame (){
 	//	SceneManager.LoadScene(1); //Lädt eine bestimmte Szene
 	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Lädt die nächste Szene in der Warteschlange, dafür müssen wir sicher gehen das diese sich dort befindet (File -> Build Settings.. -> Scenes in Build) 
+	Time.timeScale = 1f;
 	}
+	
 }

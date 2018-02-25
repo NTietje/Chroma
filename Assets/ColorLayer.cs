d178747c0f4a4b8ee0ac2d260c74b111643ec49b
cevau <cevau@posteo.de>
2018-02-25 13:24:10 +0100

    New Color Management
    - ColorSetManager: Use this in a scene to apply certain color-behaviour to the custom physical color layers. Index is the layerIndex as in settings->physics
    - Color Zone Prefab: Only one prefab for any zone. Apply it to the desired layer and it will select it's color automatically (runtime)
    - ColorLayer script: used for zones and pickups or anything else, that should belong to the color-layer-behaviour
    
    A kind of preview is in Dev Cvb Scene

---
+++ Assets/ColorLayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLayer : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		Color layerColor = Color.white;
+		foreach (LayerColor layer in LayerColors.layers) {
+			if (layer.index == gameObject.layer) {
+				layerColor = layer.color;
+				break;
+			}
+		}
+		gameObject.GetComponent<Renderer> ().material.color = layerColor;
+		Component[] renderers = gameObject.GetComponentsInChildren(typeof(Renderer));
+		//Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
+		foreach (Renderer renderer in renderers) {
+			renderer.material.color = layerColor;
+		}
+		Component[] lights = gameObject.GetComponentsInChildren (typeof(Light));
+		foreach (Light light in lights) {
+			light.color = layerColor;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

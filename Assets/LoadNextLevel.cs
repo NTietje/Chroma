f4be0e568c016408a2b17caf2ce6d653d21eed8c
taravancalster <tara.vancalster@hotmail.com>
2018-02-27 21:55:27 +0100

    added LevelCompleted to Canvas in Level1

---
+++ Assets/LoadNextLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour {
+
+	public void LoadLevel (){
+		GameManager.instance.NextLevel ();
+        Time.timeScale = 1f;		
+	}
+}

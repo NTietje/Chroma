0df6776ed16ac3b3f7edd2f2d324eae594ab7684
cevau <cevau@posteo.de>
2018-02-27 15:20:53 +0100

    Colour pick up on same layer fixed
    Colour pick ups of the same color as the player can now be picked up anyway

---
+++ Assets/DefaultLayerAnchor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultLayerAnchor : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other){
+		//Handles the pickup on the default collision layer, no matter what layer the parent object is.
+		GetComponentInParent<ColourPickUp> ().PickUp (other.gameObject);
+	}
+}

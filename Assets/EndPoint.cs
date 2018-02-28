048d8e15e931d46b93f4303b2f1d962c3eb1f5fa
taravancalster <tara.vancalster@hotmail.com>
2018-02-28 15:17:15 +0100

    added sound to EndPoint

--- Assets/EndPoint.cs
+++ Assets/EndPoint.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class EndPoint : MonoBehaviour {
 
+	public AudioClip endPointSound;
+	private AudioSource endPointSource;
+
 	// Use this for initialization
 	void Start () {
 		
+		//Looks for the AudioSource
+		endPointSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -14,8 +19,15 @@ public class EndPoint : MonoBehaviour {
 		
 	}
 	void OnTriggerEnter(Collider other){
+		
 		if (other.tag == "Player") {
-			GameManager.instance.Finish ();
+			
+		//Sound for the EndPoint	
+		endPointSource.PlayOneShot(endPointSound, .5F); //1st parameter: audio clip and 2nd paramenter: volume
+		
+		//Calls up the Finish() methode in GameManager
+		GameManager.instance.Finish ();
+		
 		}
 	}
 }
